eb9cb6d6eb7621faf635fe895335962fa84397c3
shainilayousuf <33078569+shainilayousuf@users.noreply.github.com>
2018-01-06 19:52:59 -0800

    Update chapter 6.py

--- chapter 6.py
+++ chapter 6.py
@@ -1,6 +1,7 @@
 Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
 Type "copyright", "credits" or "license()" for more information.
->>> person = {'first_name': 'shainila','last_name': 'yousuf','age': 18,'city': 'karachi'}
+>>> #6.1
+person = {'first_name': 'shainila','last_name': 'yousuf','age': 18,'city': 'karachi'}
 >>> print(person['first_name'])
 shainila
 >>> print(person['last_name'])
@@ -28,7 +29,8 @@ c's favorite number is 7.
 >>> num = favorite_numbers['d']
 >>> print("d's favorite number is " + str(num) + ".")
 d's favorite number is 1000000.
->>> glossary = {
+>>> #6.3
+glossary = {
     'string': 'A series of characters.',
     'comment': 'A note in a program that the Python interpreter ignores.',
     'list': 'A collection of items in a particular order.',
